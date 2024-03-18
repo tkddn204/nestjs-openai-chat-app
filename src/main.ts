617c7561c5e22dbde0e03aff60cc3d09470c5fee
rightpair <tkddn204@gmail.com>
2024-03-18 22:36:23 +0900

    update main.ts add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import * as bodyParser from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({}));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: process.env.npm_package_version,
+  });
+
+  app.enableCors({ origin: '*' });
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle(process.env.npm_package_name)
+    .setDescription('')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
-}
-bootstrap();
+  Logger.log(`App is running on: ${await app.getUrl()}`);
+})();
